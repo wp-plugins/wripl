e6e055e04bd52eea32db34376148bf775fa63e3f
briangallagher <briangallagher@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-01-22 16:20:38 +0000

    tagging 1.1.2
    git-svn-id: https://plugins.svn.wordpress.org/wripl/tags/1.1.2@656975 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wripl.php
+++ wripl.php
@@ -2,7 +2,7 @@
 /*
   Plugin Name: Wripl
   Description: Pluging to bring wripl's easy recomendations.
-  Version: 1.1
+  Version: 1.1.2
   Author: Brian Gallagher
   Author URI: http://wripl.com
  */
@@ -202,6 +202,8 @@ class WriplWP
     public function monitorInterests()
     {
 
+
+
         $wriplSettings = get_option('wripl_settings');
         $wriplApiBase = $this->getApiUrl();
 
@@ -242,11 +244,13 @@ class WriplWP
         }
 
 
+
+
         /**
          * If its the home page we don't need to track
-         * the user becase we don't know what exactly they are reading.
+         * the user because we don't know what exactly they are reading.
          */
-        if (!is_single()) {
+        if (!is_single() && !is_page()) {
             return;
         }
 
@@ -263,6 +267,8 @@ class WriplWP
                 return;
                 break;
         }
+
+        //die($path);
 
 
         try {
