4bb89023d4381d22b4ff0a397b8e9c3d059891df
briangallagher <briangallagher@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-03-05 10:15:53 +0000

    Adding some debug code on the wripl connect page. Updating to tag 1.2.4.
    git-svn-id: https://plugins.svn.wordpress.org/wripl/trunk@676366 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- connect.php
+++ connect.php
@@ -7,35 +7,47 @@ require_once(dirname(dirname(dirname(dirname(__FILE__)))) . '/wp-load.php');
 $wriplWP = WriplWP::$instance;
 
 if (!$wriplWP->isSetup()) {
-    exit();
+    header($_SERVER['SERVER_PROTOCOL'] . ' 500 Internal Server Error', true, 500);
+    exit('wripl doesn\'t seem to be set up yet');
 }
 
-$wriplSettings = get_option('wripl_settings');
-
-$wriplApiBase = $wriplWP->getApiUrl();
-$consumerKey = $wriplSettings['consumerKey'];
-$consumerSecret = $wriplSettings['consumerSecret'];
-
-$config['apiBaseUrl'] = $wriplApiBase;
-$config['oauthBaseUrl'] = Wripl_Client::getOauthUrlFromApiUrl($wriplApiBase);
-
-$client = new Wripl_Client(new Wripl_Oauth_Client_Adapter_OAuthSimple($consumerKey, $consumerSecret), $config);
-
-/**
- * Hinky work around for a php bug on OSX Mountain Lion & apc (I think)
- * https://bugs.php.net/bug.php?id=60017&thanks=6
- */
-//$callbackUrl = plugins_url() . '/' . basename(dirname((__FILE__))) . '/callback.php';
-$callbackUrl = plugins_url('callback.php', __FILE__);
-
 try {
+
+    $wriplSettings = get_option('wripl_settings');
+
+    $wriplApiBase = $wriplWP->getApiUrl();
+    $consumerKey = $wriplSettings['consumerKey'];
+    $consumerSecret = $wriplSettings['consumerSecret'];
+
+    $config['apiBaseUrl'] = $wriplApiBase;
+    $config['oauthBaseUrl'] = Wripl_Client::getOauthUrlFromApiUrl($wriplApiBase);
+
+    $client = new Wripl_Client(new Wripl_Oauth_Client_Adapter_OAuthSimple($consumerKey, $consumerSecret), $config);
+
+    /**
+     * Hinky work around for a php bug on OSX Mountain Lion & apc (I think)
+     * https://bugs.php.net/bug.php?id=60017&thanks=6
+     */
+    //$callbackUrl = plugins_url() . '/' . basename(dirname((__FILE__))) . '/callback.php';
+    $callbackUrl = plugins_url('callback.php', __FILE__);
+
     $requestToken = $client->getRequestToken($callbackUrl);
 
     $wriplWP->storeRequestToken($requestToken);
     $wriplWP->storeOauthRefererUrl($_SERVER['HTTP_REFERER']);
 
     $client->authorize();
+
 } catch (Exception $e) {
+
+    header($_SERVER['SERVER_PROTOCOL'] . ' 500 Internal Server Error', true, 500);
+
+    if (isset($_GET['debug'])) {
+        echo json_encode(array(
+            'message' => $e->getMessage(),
+            'code' => $e->getCode(),
+        ));
+    }
+
     error_log('message : ' . $e->getMessage() . ' code: ' . $e->getCode());
 }
-?>
--- wripl.php
+++ wripl.php
@@ -2,7 +2,7 @@
 /*
   Plugin Name: Wripl
   Description: Pluging to bring wripl's easy recomendations.
-  Version: 1.2.3
+  Version: 1.2.4
   Author: Brian Gallagher
   Author URI: http://wripl.com
  */
