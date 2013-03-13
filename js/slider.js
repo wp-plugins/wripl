bd21c30a7c64a0455b68de6fb3f3117e637bbcb9
briangallagher <briangallagher@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-03-13 15:07:17 +0000

    Added Handlebar reference
    git-svn-id: https://plugins.svn.wordpress.org/wripl/trunk@681280 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/slider.js
+++ js/slider.js
@@ -1,89 +1,91 @@
-jQuery(document).ready(function ($) {
+(function ($, Handlebars) {
 
-    var sliderRevealed;
-    var defaultPosition = -370;
-    var displayAtPercent = 10;
+    jQuery(document).ready(function ($) {
 
-    var slider = $("<div id='wripl-slider'><img src='/slide-out-from-side-mockup-withdropshadow.png'></div>")
-        .css({
-            position:'fixed',
-            bottom:20,
-            //left:$(this).width() - 50 + 'px'
-            right: defaultPosition
-        });
+        var sliderRevealed;
+        var defaultPosition = -370;
+        var displayAtPercent = 10;
 
-
-    $('body').append(slider);
-
-    // Add listeners
-    $("body").bind( "wripl-ajax-init-not-logged-in" , function (e, params) {
-        console.log("Not logged in!");
-        console.log(e);
-        $.get( WriplAjaxProperties.pluginPath + 'handlebar-templates/slider/inactive.html', function(data) {
-
-            template = Handlebars.compile(data);
-            compiledHtml = template({
-                wriplWidgetProperties: WriplWidgetProperties,
-                wriplAjaxProperties: WriplAjaxProperties
+        var slider = $("<div id='wripl-slider'><img src='/slide-out-from-side-mockup-withdropshadow.png'></div>")
+            .css({
+                position:'fixed',
+                bottom:20,
+                //left:$(this).width() - 50 + 'px'
+                right:defaultPosition
             });
 
-            $('#wripl-slider').html(compiledHtml);
-        });
-    });
+        $('body').append(slider);
 
-    $("body").bind( "wripl-ajax-init-logged-in" , function (e, params) {
-        console.log("Logged in!");
-        console.log(params);
-        recommendationsArray = params.recommendations;
+        // Add listeners
+        $("body").bind("wripl-ajax-init-not-logged-in", function (e, params) {
+            console.log("Not logged in!");
+            console.log(e);
+            $.get(WriplAjaxProperties.pluginPath + 'handlebar-templates/slider/inactive.html', function (data) {
 
-        if( WriplWidgetProperties.maxRecommendations ){
-            recommendationsArray = recommendationsArray.slice(0 , WriplWidgetProperties.maxRecommendations);
-        }
+                template = Handlebars.compile(data);
+                compiledHtml = template({
+                    wriplWidgetProperties:WriplWidgetProperties,
+                    wriplAjaxProperties:WriplAjaxProperties
+                });
 
-        $.get( WriplAjaxProperties.pluginPath + 'handlebar-templates/slider/active.html', function(data) {
-            template = Handlebars.compile(data);
-            compiledHtml = template({
-                wriplWidgetProperties: WriplWidgetProperties,
-                wriplAjaxProperties: WriplAjaxProperties,
-                recommendations: recommendationsArray
+                $('#wripl-slider').html(compiledHtml);
             });
+        });
 
-            $('#wripl-slider').html(compiledHtml);
+        $("body").bind("wripl-ajax-init-logged-in", function (e, params) {
+            console.log("Logged in!");
+            console.log(params);
+            recommendationsArray = params.recommendations;
+
+            if (WriplWidgetProperties.maxRecommendations) {
+                recommendationsArray = recommendationsArray.slice(0, WriplWidgetProperties.maxRecommendations);
+            }
+
+            $.get(WriplAjaxProperties.pluginPath + 'handlebar-templates/slider/active.html', function (data) {
+                template = Handlebars.compile(data);
+                compiledHtml = template({
+                    wriplWidgetProperties:WriplWidgetProperties,
+                    wriplAjaxProperties:WriplAjaxProperties,
+                    recommendations:recommendationsArray
+                });
+
+                $('#wripl-slider').html(compiledHtml);
+            });
+        });
+
+
+        $(document).scroll(function () {
+
+            var scrollAmount = $(window).scrollTop();
+            var documentHeight = $(document).height();
+            var scrollPercent = (scrollAmount / documentHeight) * 100;
+
+            var showSlider = function () {
+                slider.animate(
+                    {
+                        right:20
+                    }
+                );
+            }
+
+            var hideSlider = function () {
+                slider.animate(
+                    {
+                        right:defaultPosition
+                    }
+                );
+            }
+
+            if (scrollPercent > displayAtPercent && !sliderRevealed) {
+                showSlider();
+                sliderRevealed = true;
+            }
+
+            if (scrollPercent < displayAtPercent && sliderRevealed) {
+                hideSlider();
+                sliderRevealed = false;
+            }
+
         });
     });
-
-
-    $(document).scroll(function () {
-
-        var scrollAmount = $(window).scrollTop();
-        var documentHeight = $(document).height();
-        var scrollPercent = (scrollAmount / documentHeight) * 100;
-
-        var showSlider = function () {
-            slider.animate(
-                {
-                    right: 20
-                }
-            );
-        }
-
-        var hideSlider = function () {
-            slider.animate(
-                {
-                    right: defaultPosition
-                }
-            );
-        }
-
-        if (scrollPercent > displayAtPercent && !sliderRevealed) {
-            showSlider();
-            sliderRevealed = true;
-        }
-
-        if (scrollPercent < displayAtPercent && sliderRevealed) {
-            hideSlider();
-            sliderRevealed = false;
-        }
-
-    });
-});
+})(jQuery, Handlebars);
--- js/widget.js
+++ js/widget.js
@@ -1,66 +1,4 @@
-(function ($) {
-
-    /**
-     * Gets and renders the widget.
-     */
-//    var getWidget = function () {
-//
-//        jQuery.post(
-//            WriplAjax.ajaxUrl,
-//            {
-//                action:'wripl-get-widget-recommendations',
-//                maxRecommendations:WriplAjax.maxRecommendations
-//            },
-//            function (response) {
-//                jQuery("div#wripl-ajax-container").html(response);
-//            }
-//        );
-//    }
-//
-//    /**
-//     * Fetches the activity code from the host site,
-//     * on success it starts the tracker and gets the widget content.
-//     */
-//    var beginTracking = function () {
-//
-//        jQuery.post(
-//            WriplAjax.ajaxUrl,
-//            {
-//                action:'wripl-get-activity-code',
-//                path:WriplAjax.path
-//            },
-//            function (response) {
-//
-//                if (response.activityHashId && response.endpoint) {
-//                    wripl.main(response);
-//                }
-//
-//                if (response.piwikScript) {
-//                    var script = document.createElement('script');
-//                    script.type = 'text/javascript';
-//                    script.src = response.piwikScript;
-//
-//                    jQuery("body").append(script);
-//                }
-//
-//                getWidget();
-//
-//            }
-//        );
-//    }
-//
-//    jQuery(document).ready(function () {
-//
-//        if (typeof WriplAjax.path != 'undefined') {
-//            beginTracking();
-//        } else {
-//            getWidget();
-//        }
-//    });
-
-    /**
-     * NEW
-     */
+(function ($, Handlebars) {
 
     jQuery(document).ready(function () {
 
@@ -107,4 +45,4 @@
 
     });
 
-})(jQuery);
+})(jQuery, Handlebars);
