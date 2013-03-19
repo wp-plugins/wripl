3b731f3bb81b0ecdae467432e0d711c41328b49a
briangallagher <briangallagher@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-03-19 15:33:53 +0000

    removing unnecessary reference to widget in slider.js
    git-svn-id: https://plugins.svn.wordpress.org/wripl/trunk@684271 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wripl.php
+++ wripl.php
@@ -2,7 +2,7 @@
 /*
   Plugin Name: Wripl
   Description: Pluging to bring wripl's easy recomendations.
-  Version: 1.3.3
+  Version: 1.3.4
   Author: Brian Gallagher
   Author URI: http://wripl.com
  */
@@ -21,7 +21,7 @@ class WriplWP
     const ITEM_NEEDS_INDEXING = -1;
     const ITEM_QUEUED = 0;
     const ITEM_INDEXED = 1;
-    const VERSION = '1.3.3';
+    const VERSION = '1.3.4';
 
     public $wriplPluginHelper;
 
