7bca6d132784135ef40b6e1b8de3f291310327f5
briangallagher <briangallagher@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-04-18 16:27:58 +0000

    Version bump
    git-svn-id: https://plugins.svn.wordpress.org/wripl/trunk@699866 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wripl.php
+++ wripl.php
@@ -2,7 +2,7 @@
 /*
   Plugin Name: Wripl
   Description: Pluging to bring wripl's easy recomendations.
-  Version: 1.3.12
+  Version: 1.3.13
   Author: Wripl
   Author URI: http://wripl.com
  */
@@ -26,7 +26,7 @@ class WriplWP
     const ITEM_NEEDS_INDEXING = -1;
     const ITEM_QUEUED = 0;
     const ITEM_INDEXED = 1;
-    const VERSION = '1.3.12';
+    const VERSION = '1.3.13';
 
     public $wriplPluginHelper;
 
