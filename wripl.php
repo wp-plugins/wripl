4013a309867cdb6d928c323a78d5b58d84a84baa
briangallagher <briangallagher@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-10-25 09:52:39 +0000

    Tagging version 1.5.0
    git-svn-id: https://plugins.svn.wordpress.org/wripl/tags/1.5.0@793495 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wripl.php
+++ wripl.php
@@ -2,7 +2,7 @@
 /*
   Plugin Name: Wripl
   Description: Plugin to bring wripl's easy recommendations to Wordpress.
-  Version: 1.4.2
+  Version: 1.5.0
   Author: Wripl
   Author URI: http://wripl.com
  */
@@ -26,7 +26,7 @@ class WriplWP
     const ITEM_NEEDS_INDEXING = -1;
     const ITEM_QUEUED = 0;
     const ITEM_INDEXED = 1;
-    const VERSION = '1.4.2';
+    const VERSION = '1.5.0';
 
     public $wriplPluginHelper;
 
