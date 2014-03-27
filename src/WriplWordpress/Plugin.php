541ab091ba4786df5cdf847383412df43da91a83
briangallagher <briangallagher@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-03-27 10:26:14 +0000

    Deploying the new version
    git-svn-id: https://plugins.svn.wordpress.org/wripl/trunk@882857 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- src/WriplWordpress/Plugin.php
+++ src/WriplWordpress/Plugin.php
@@ -2,7 +2,7 @@
 
 class WriplWordpress_Plugin
 {
-    const VERSION = '1.6.1';
+    const VERSION = '1.6.2';
 
     static $instance;
 
--- wripl.php
+++ wripl.php
@@ -2,7 +2,7 @@
 /*
   Plugin Name: Wripl
   Description: Plugin to bring wripl's easy recommendations to Wordpress.
-  Version: 1.6.1
+  Version: 1.6.2
   Author: Wripl
   Author URI: http://wripl.com
  */
